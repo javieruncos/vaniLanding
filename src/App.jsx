c61a2c6d1f4115b99522db0ba3039afe00247762
Javier Uncos <javiersrfc30@gmail.com>
2024-09-05 13:29:24 -0300

    estructuras de carpetas

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+
+  return (
+    <>
+      <div>
+       <h1>Landing Page</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
