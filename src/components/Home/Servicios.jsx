e8cffed07a325e6d750c98c2ef19b4f85f3dd163
Javier Uncos <javiersrfc30@gmail.com>
2024-12-27 01:09:38 -0300

    agregamos fondo de emoji a las secciones

--- src/components/Home/Servicios.jsx
+++ src/components/Home/Servicios.jsx
@@ -56,7 +56,7 @@ const Servicios = () => {
                     <div className="col-12 col-md-6 col-lg-6">
                         <div className="servicios-img" data-aos="fade-right">
                             <img src={Estefi} alt="" />
-
+                            
                         </div>
                     </div>
                     <div className="col-12 col-md-6 col-lg-6">
